6f6f787c67417f70d7b7eb181732120a0bffe413
Kero-the-Hero <syashley1119@gmail.com>
2024-03-20 15:41:45 +0900

    #6.5 more practice pt 1

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,14 +3,19 @@ import {
     watch,
     getEdit,
     postEdit,
+    getUpload,
+    postUpload,
     deleteVideo,
-    upload,
+    upload,    
 } from '../controllers/videoController.js';
 
 const videoRouter = express.Router();
 
 videoRouter.get('/:id(\\d+)', watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-
+// videoRouter.get("/upload",getUpload);
+// videoRouter.post("/upload",postUpload);
+// 아래 한 줄로 위의 두 줄을 요약!
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
