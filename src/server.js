da9e3074a355af2ce7de638e4740cbbaeceabf22
Kero-the-Hero <syashley1119@gmail.com>
2024-03-21 06:52:09 +0900

    #6.11 query pt 1

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
-import "./db.js";
-import Video from "./models/Video.js";
 import express from 'express';
 import morgan from 'morgan';
 
 import globalRouter from './routers/globalRouter.js';
 import userRouter from './routers/userRouter.js';
 import videoRouter from './routers/videoRouter.js';
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan('dev');
@@ -20,7 +16,4 @@ app.use('/', globalRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
 
-const handleListening = () =>
-    console.log(`✅ Server is Listening on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
