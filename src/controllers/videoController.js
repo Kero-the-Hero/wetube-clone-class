8b47c25e36bec81e84824d4a029f9a64f46bd786
Kero-the-Hero <syashley1119@gmail.com>
2024-03-21 07:11:38 +0900

    #6.12 query pt 2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,21 @@
 import Video from "../models/Video";
 
 export const home = (req, res) => {
-    Video.find( {},(error,videos)=>{});
-    return res.render('home', { pageTitle: 'Home' });
+    console.log("Start");
+    Video.find({}, (error,videos) => {
+    //     console.log("error", error);
+    //     console.log("videos", videos);
+    // });
+    // console.log("hello");
+        console.log("Finished");
+        return res.render('home', { pageTitle: 'Home', videos: [] });
+    });
+    console.log("I finish first");
 };
+// 위와 같이 기술하면 console.log의 서순은
+// Start -> I finish first -> Finished의 순이 된다.
+// connection을 하고나서 render 들어가기 때문
+
 export const watch = (req, res) => {
     const { id } = req.params;
     return res.render('watch', { pageTitle: `Watching` });
