c91e1213736681602e5431c1c7e3de3d7d7546f2
Kero-the-Hero <syashley1119@gmail.com>
2024-03-26 08:51:13 +0900

    #6.17 exceptions and validation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -34,15 +34,18 @@ export const getUpload = (req,res) => {
 export const postUpload = async (req,res) => {
     //Here we will add a video to the videos array.
     const {title, description, hashtags} = req.body;
-    await Video.create({
-        title,
-        description,
-        createdAt: Date.now(),
-        hashtags:hashtags.split(",").map((word)=>`#${word}`),
-        meta : {
-            views: 0,
-            rating : 0,
-        },
-    });
-    return res.redirect("/");
+    try {
+        await Video.create({
+            title,
+            description,
+            hashtags:hashtags.split(",").map((word)=>`#${word}`),
+        });
+        return res.redirect("/");
+    } catch (error){
+      return res.render("upload", {
+          pageTitle: "Upload video", 
+          errorMessage: error._message,
+        });
+    };
+ 
 };
