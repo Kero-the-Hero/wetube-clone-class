f578291c52072c7328c0607f0fbe42be62595f70
Kero-the-Hero <syashley1119@gmail.com>
2024-03-26 15:31:58 +0900

    #6.25 delete video_use delete! not remove

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,17 +5,14 @@ import {
     postEdit,
     getUpload,
     postUpload,
-    deleteVideo,
-    upload,    
+    deleteVideo,   
 } from '../controllers/videoController.js';
 
 const videoRouter = express.Router();
 
 videoRouter.get('/:id([0-9a-f]{24})', watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-// videoRouter.get("/upload",getUpload);
-// videoRouter.post("/upload",postUpload);
-// 아래 한 줄로 위의 두 줄을 요약!
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
