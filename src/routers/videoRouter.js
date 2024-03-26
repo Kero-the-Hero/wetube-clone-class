656a6c53708635f587c274bad3f1d0834ddf03eb
Kero-the-Hero <syashley1119@gmail.com>
2024-03-26 11:15:38 +0900

    #6.19 video detail_findById

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -11,8 +11,8 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get('/:id(\\d+)', watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get('/:id([0-9a-f]{24})', watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 // videoRouter.get("/upload",getUpload);
 // videoRouter.post("/upload",postUpload);
 // 아래 한 줄로 위의 두 줄을 요약!
